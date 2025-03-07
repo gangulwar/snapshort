68bff2b138fbc24e232b867975edde61b75d1457
Aarsh Gangulwar <aarshgangulwar23@gmail.com>
2025-03-08 00:26:17 +0530

    add mongodb dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,11 @@ repositories {
 
 dependencies {
     implementation(libs.ktor.server.core)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.mongodb.driver.core)
+    implementation(libs.mongodb.driver.sync)
+    implementation(libs.bson)
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.config.yaml)
